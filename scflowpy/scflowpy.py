9bb76ad4edfa9c61eec4615e6c0595290c4762b1
CK <31043044+thecombiz@users.noreply.github.com>
2021-11-24 08:26:33 +0000

    Outlining read_sce fn

--- scflowpy/scflowpy.py
+++ scflowpy/scflowpy.py
@@ -1,1 +1,22 @@
-"""Main module."""
+"""Read a SingleCellExperiment Object from scFlow
+A module with 'read_sce' that imports a
+SCE generated with the R package scFlow
+Author: Combiz Khozoie, Ph.D. <c.khozoie@imperial.ac.uk>
+Created: 22 November, 2021
+"""
+# imports
+import sys           # for exit execution
+import pandas as pd  # to handle tsv files
+
+# modules
+def read_sce(folder_path):
+    """Description
+    Parameters
+    ---------
+    <parameter_1>: <data_type>
+        <parameter_1_description>
+    Returns
+    ---------
+    <output_1>: <data_type>
+        <output_1_description>
+    """
---
+++ scflowpy/template.py
@@ -0,0 +1,16 @@
+"""<Short description>
+<Long description>
+Author: <Name> <email>
+Created: <date>
+"""
+
+"""Description
+Paramters
+---------
+<parameter_1>: <data_type>
+    <parameter_1_description>
+Returns
+---------
+<output_1>: <data_type>
+    <output_1_description>
+"""
