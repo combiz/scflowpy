c6a66001784314d00ccfa65fd0163c3ec6c5609c
CK <31043044+thecombiz@users.noreply.github.com>
2022-05-17 22:52:54 +0100

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,17 @@ setup(
     author="Combiz Khozoie",
     author_email='c.khozoie@imperial.ac.uk',
     python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
     description="Python helper functions for scFlow",
     entry_points={
         'console_scripts': [
